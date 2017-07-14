3e663d3813e016161af6b8f510345ca76efcce6b
YohanPipereau <yohan.pipereau@gmail.com>
2017-07-14 13:46:33 +0200

    patch python 2 and 3 error

--- SocketConnect.py
+++ SocketConnect.py
@@ -1,44 +1,26 @@
-import socket, time, threading, sys
-
-class SocketSend(threading.Thread):
-
-
-import os
-import time
-import sys
+import os, socket, time, sys
 from threading import Thread
-import socket
 
-
-def SocketConnect():
+def SocketConnect(message,unixsocket_path = '/var/run/openvassd.sock'):
     #unixsocket_path is the socket of openvassd which it uses to communicate with redis and any manager
-    unixsocket_path = '/var/run/openvassd.sock'
 
-    ##Check the existence of the socket
     try:
+        #Check the existence of the socket
         os.path.isfile(unixsocket_path)
     except OSError:
         print(" This unixsocket does not exist ... default is /var/run/openvassd.sock ")
 
     ##Instantiate the socket and connect the client to it (the server is openvas-scanner)
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    print >>sys.stderr, 'connecting to %s' % unixsocket_path #make sure any input goes to sderr
-    try:
-        sock.connect(unixsocket_path)
-    except socket.error, msg:
-        print >>sys.stderr, msg
-        sys.exit(1)
+    #print >>sys.stderr, 'connecting to %s' % unixsocket_path #make sure any input goes to sderr
+    sock.connect(unixsocket_path)
 
-    ##Writing and Reading in the socket
-    #Writing in the socket
-    #message = "CLIENT <|> NVT_INFO"
     def send_msg(sock):
-        for line in open('25478.client'):
+        for line in message:
             sock.send(line)
             sys.stdout.write(line)
             time.sleep(0.1)
 
-    #Reading in the socket$
     def recv_msg(sock):
         while True:
             data = sock.recv(1024)
