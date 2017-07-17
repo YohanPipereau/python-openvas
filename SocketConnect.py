7cf9fdb40bfffde8f5d95bcd2af586d9818c3ca8
YohanPipereau <yohan.pipereau@gmail.com>
2017-07-17 12:20:04 +0200

    Remove timer for every step of while leap and use signal instead

--- SocketConnect.py
+++ SocketConnect.py
@@ -1,5 +1,6 @@
 import os, socket, time, sys
 from threading import Thread
+import threading
 
 def SocketConnect(message,unixsocket_path = '/var/run/openvassd.sock'):
     #unixsocket_path is the socket of openvassd which it uses to communicate with redis and any manager
@@ -14,16 +15,25 @@ def SocketConnect(message,unixsocket_path = '/var/run/openvassd.sock'):
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     #print >>sys.stderr, 'connecting to %s' % unixsocket_path #make sure any input goes to sderr
     sock.connect(unixsocket_path)
+    global event
+    event = threading.Event()
 
     def send_msg(sock,message):
         for line in message.splitlines(True):
+	    global event
+	    event.set()
 	    sock.send(line)
             sys.stdout.write(line)
-            time.sleep(.05)
+	    time.sleep(0.1) #wait a bit cause the receiver is a bit long to write
+	    event.clear()
+            #time.sleep(.05)
+	    event.set()
 
     def recv_msg(sock):
 	outputVar = ""
         while True:
+	    global event
+	    event.wait() #block sender until receiver send set
             data = sock.recv(1024)
             sys.stdout.write(data)
 	    outputVar = outputVar + data
