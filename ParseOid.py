4ef8a05a63980114eb37e656790296b19044795c
YohanPipereau <yohan.pipereau@gmail.com>
2017-07-18 15:24:47 +0200

    Listing families is now working

--- ParseOid.py
+++ ParseOid.py
@@ -5,23 +5,25 @@ class ParseOid(Parse):
     def __init__(self,parserMatch,scanData):
         self.scanData = scanData
         self.parserMatch = parserMatch #when parserMatch is detected -> match section begins
+	self.familyArray = []
 
     def Parser(self,line):
 	oidList=line.split("<|>")
-	#Create an array with Family as column and oid as lines
-	#Array = [[Family1Name,oid1.1,oid1.2,...],[Family2Name,oid2.1,oid2.2,...],...]
-	familyFound = False #boolean equals False as long as we did not add the oidLine family to the array
-	k=0
-	while k<len(familyArray) and familyFound == False :
-	    if oidList[4] == familyArray[i][0] :
-		#The family of this oid is already in the array
-		familyArray[i].append(oidList[0])
-		familyFound = True
-	if familyFound == False:
-	    #If we did not find the family then we need to append it to the array
-	    familyArray.append([oidList[4],oidList[0]])
-	return(familyArray)
-
+	if len(oidList) == 10 :
+	    #Create an array with Family as column and oid as lines
+	    #Array = [[Family1Name,oid1.1,oid1.2,...],[Family2Name,oid2.1,oid2.2,...],...]
+	    familyFound = False #boolean equals False as long as we did not add the oidLine family to the array
+	    k=0
+	    while k<len(self.familyArray) and familyFound == False :
+	        if oidList[4] == self.familyArray[k][0]:
+		    #The family of this oid is already in the array
+		    self.familyArray[k].append(oidList[0])
+		    familyFound = True
+		k+=1
+	    if familyFound == False:
+	        #If we did not find the family then we need to append it to the array
+		print(oidList[4]) #print the families names
+	        self.familyArray.append([oidList[4],oidList[0]])
 
 
 #oid <|> Name of NVT <|> infos <|> Licence of vulnerability <|> Family <|> ID of revision <|> CVE id <|> BID (bugtrack id) <|> URL <|> Description \n
