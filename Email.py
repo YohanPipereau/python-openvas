66e80f39d5c550fc798af51b672cc54e82367bf9
YohanPipereau <yohan.pipereau@gmail.com>
2017-07-21 11:20:08 +0200

    Add Subject field for single destination

--- Email.py
+++ Email.py
@@ -12,6 +12,8 @@ class Email:
     #Parsing the header: From, To, Subject
     def sendEmail(self):
 	smtpObj = smtplib.SMTP('127.0.0.1')
+	self.msg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n"
+       % ("openvas@cern.ch", ", ".join(self.destinationAddr), "OpenVAS report")) + self.msg
 	smtpObj.sendmail("openvas@cern.ch", self.destinationAddr,self.msg)
 	smtpObj.quit() #end the SMTP connection
 
