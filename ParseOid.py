a1bab57af7fe9fcc81cd94d95112f197490855e5
YohanPipereau <yohan.pipereau@gmail.com>
2017-07-31 14:25:19 +0200

    Add error for oidList of size different than 10

--- ParseOid.py
+++ ParseOid.py
@@ -3,19 +3,18 @@
 class ParseOid():
     def __init__(self,scanData):
         self.scanData = scanData
-        self.parserMatch = "SERVER <|> PLUGIN_LIST <|>\n"
         self.familyDict = {}
 
     def SectionParser(self):
         scanList = self.scanData.splitlines(True)
         parsingTrigger = False
         for line in scanList:
-            if line == self.parserMatch:
+            if line == "SERVER <|> PLUGIN_LIST <|>\n":
                 parsingTrigger = True
-            if parsingTrigger == True: #Every object has a parser which is called if we detect a line which match parserMatch
-                    self.Parser(line)
-            if line == "<|> SERVER": #Detect end of the matching section
-                parsingTrigger = False
+            elif line == "<|> SERVER\n": #Detect end of the matching section
+		return 0
+            elif parsingTrigger == True: #Every object has a parser which is called if we detect a line which match parserMatch
+                self.Parser(line)
 
     def Parser(self,line):
         oidList=line.split(" <|> ")
@@ -23,14 +22,15 @@ class ParseOid():
             familyFound = False #boolean equals False as long as we did not add the oidLine family to the array
             k=0
             while k<len(self.familyDict.keys()) and familyFound == False: #is the oid family in the family dict?
-                if oidList[4] == self.familyDict.keys()[k]:
-                    #The family of this oid is already in the array
+                if oidList[4] == self.familyDict.keys()[k]:#The family of this oid is already in the array
                     self.familyDict[self.familyDict.keys()[k]].update( {oidList[0] : {"name" : oidList[1] , "description" : oidList[9]}})
                     familyFound = True
                 k+=1
-            if familyFound == False:
-                #If we did not find the family then we need to append the family to the dict
+            if familyFound == False:#If we did not find the family then we need to append the family to the dict
                 self.familyDict.update({oidList[4] : {oidList[0] : { "name" : oidList[1], "description" : oidList[9]}}})
+	else:
+	    print("Error! oidList has a size different from 10 characters.")
+	    sys.exit(2)
 
 
 #oid <|> Name of NVT <|> infos <|> Licence of vulnerability <|> Family <|> ID of revision <|> CVE id <|> BID (bugtrack id) <|> URL <|> Description \n
