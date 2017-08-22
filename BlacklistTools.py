505eebe8a2a8b5de85af0213d1c5f16f616caa30
YohanPipereau <yohan.pipereau@gmail.com>
2017-08-22 17:22:56 +0200

    add & remove oid to blacklist.conf file

--- BlacklistTools.py
+++ BlacklistTools.py
@@ -31,19 +31,57 @@ class BlacklistTools:
 		oidBID = self.familyDict[oidFamily][oid]['BID']
 		oidURL = self.familyDict[oidFamily][oid]['URL']
 		message = """
-    ** {0} , {1} **
-    * Family of Vulnerability : {2}
-    * CVE : {3}
-    * BID : {4}
-    * URL : {5}
-    * Description: {6}""".format(oidName, oidNumber, oidFamily, oidCVE, oidBID, oidURL, oidDescription)
+** {0} , {1} **
+* Family of Vulnerability : {2}
+* CVE : {3}
+* BID : {4}
+* URL : {5}
+* Description: {6}""".format(oidName, oidNumber, oidFamily, oidCVE, oidBID, oidURL, oidDescription)
 		print(message)
 	    except KeyError:
 		print(Color.BLUE + oid.strip() + ' is blacklisted but can be removed safely because it is an ancien plugin.' + Color.END)
 	return 0
+
+    def searchOid(self, oid):
+	"""
+	    Search oid in conf/blacklist.conf. Return True/False if Found/not found
+	    and also position of oid in File.
+	"""
+	with open('conf/blacklist.conf', 'r') as blacklistFile:
+	   blacklist = blacklistFile.readlines() 
+	for rank in range(len(blacklist)):
+	    print(rank)
+	    if blacklist[rank].strip() == oid.strip():
+		return True, rank
+	return False, None
 	
-    def AddOid(self, number):
+    def AddOid(self, oidList):
 	"""
 	    Function used to add a new oid to conf/blacklist.conf.
 	"""
-	pass
+	for oid in oidList:
+	    isFound, _ = self.searchOid(oid)
+	    if isFound:
+	       print(Color.RED + oid.strip() + ' Already in blacklist.conf.' + Color.END) 
+	    else:
+		with open('conf/blacklist.conf', 'w') as blacklistFile:
+		    blacklistFile.write(oid) 
+		print(Color.GREEN + oid.strip() + ' added to blacklist.conf' + Color.END)
+	return 0
+
+    def RemoveOid(self, oidList):
+	"""
+	    Function used to remove an oid in conf/blacklist.conf
+	"""
+	for oid in oidList:
+	    isFound , index = self.searchOid(oid)
+	    if isFound:
+		with open('conf/blacklist.conf', 'r') as blacklistFile:
+		    wholeFile = blacklistFile.readlines()
+		wholeFile.pop(index)
+		with open('conf/blacklist.conf', 'w+') as blacklistFile:
+		    for line in wholeFile:
+			blacklistFile.write(line)
+		print(Color.GREEN + oid.strip() + ' removed from blacklist.conf' + Color.END)
+	    else:
+		print(Color.RED + oid.strip() + ' is not in blacklist.conf' + Color.END)
