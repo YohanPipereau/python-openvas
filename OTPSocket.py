ac6c2abe80e7f7447901b0cead3f6ee2e9475833
YohanPipereau <yohan.pipereau@gmail.com>
2017-08-23 16:28:21 +0200

    add a bash script to fix the redis bug of openvas scanner

--- OTPSocket.py
+++ OTPSocket.py
@@ -43,6 +43,7 @@ class OTPSocket:
 	    while not '<|> SERVER' in last + cur:
 		last, cur = cur, self.sock.recv(8192)
 		acc += self.remain.strip() + cur
+		print(acc)
 	(useNow, self.remain) = acc.split('<|> SERVER',1)
 	if '<|> BYE' in cur:
 	    self.stop = True
